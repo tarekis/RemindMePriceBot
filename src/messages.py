343a78a00529313d9f49ddfd0c82f06e4c2102e1
Clemens Himmer <clemens.himmer@fusonic.net>
2021-02-26 11:27:21 +0100

    print user name

--- src/messages.py
+++ src/messages.py
@@ -2,7 +2,8 @@ import database
 
 
 def finish_task(conn, task_id):
-    print("TODO Implement: Should send messages to the subscribers")
     subscribers = database.get_subscribers(conn, task_id)
     print(subscribers)
+    for subscriber in subscribers:
+        print("Message {subscriber} that {task_id} is finished")
     database.remove_task(conn, task_id)
