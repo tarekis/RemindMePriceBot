b92798bbc7ca3030f4b514fd9b45bddbe4aa7595
Clemens Himmer <clemens.himmer@fusonic.net>
2021-02-26 11:53:19 +0100

    logging

--- src/messages.py
+++ src/messages.py
@@ -1,4 +1,5 @@
 import database
+from datetime import datetime
 
 
 def finish_task(conn, task_id):
@@ -6,11 +7,15 @@ def finish_task(conn, task_id):
     task_details = database.get_task_details(conn, task_id)
 
     symbol, target, direction_is_up, before_condition = task_details
+    print("\n")
+    print("Task details: " + str(task_id))
     print(subscribers)
     print(symbol)
     print(target)
     print(direction_is_up)
     print(before_condition)
+    print(before_condition == datetime.max)
+    print("\n")
 
     for subscriber in subscribers:
         print("Message {subscriber} that {task_id} is finished")
