d459e17d9ff0df56bbf524da72427d148298b116
Clemens Himmer <clemens.himmer@fusonic.net>
2021-02-26 09:52:59 +0100

    remove underscore from command

--- src/static.py
+++ src/static.py
@@ -2,7 +2,7 @@ from decouple import config
 
 ENVIRONMENT = config('environment')
 # RemindMeBot doesn't like me, so lets use that for now :(
-COMMAND = "!RemindMiWhenPrice_TEST"
+COMMAND = "!RemindMiWhenPriceTEST"
 COMMAND_LOWER = COMMAND.lower()
 REDDIT_USERNAME = "RemindMePriceBot"
 API_URL = "https://beta.pushshift.io/search/reddit/comments/"
