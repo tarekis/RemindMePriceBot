6c7548bd3481cc9cc977a9747aa3e769422005c9
Clemens Himmer <clemens.himmer@fusonic.net>
2021-03-02 18:35:11 +0100

    reply with context when messaging

--- src/messages.py
+++ src/messages.py
@@ -19,21 +19,22 @@ def finish_task(conn, reddit, task_id, trigger_price):
         task_details = database.get_task_details(conn, task_id)
 
         symbol, target, direction_is_up, currency, before_condition = task_details
+        parent_comment = reddit.comment(source_comment_id)
+
+        subject = f"{symbol} {direction_string_past_tens} {target} {currency}"
+
+        before_string = "" if (before_condition == datetime.max) else f" before {before_condition}"
+        direction_string_present_tense = "hits" if direction_is_up else "drops to"
+        direction_string_past_tens = "hit" if direction_is_up else "dropped to"
+        trigger_string = "high" if direction_is_up else "low"
+        emoji = "📈" if direction_is_up else "📉"
 
         for subscriber_tuple in subscribers:
-            before_string = "" if (before_condition == datetime.max) else f" before {before_condition}"
-            direction_string_present_tense = "hits" if direction_is_up else "drops to"
-            direction_string_past_tens = "hit" if direction_is_up else "dropped to"
-            trigger_string = "high" if direction_is_up else "low"
-            emoji = "📈" if direction_is_up else "📉"
-
-            subject = f"{symbol} {direction_string_past_tens} {target} {currency}"
-
-            print("Should reply that it was because of this parent comment: " + str(source_comment_id))
 
             message_builder = []
             message_builder.append(f"You asked me to remind you when {symbol} {direction_string_present_tense} {target} {currency}{before_string}.\n\n")
             message_builder.append(f"{symbol} {direction_string_past_tens} {target} {currency} today with the current day {trigger_string} at {trigger_price}. {emoji}\n\n")
+            message_builder.append(f"You requested the reminder in [this context](https://www.reddit.com{parent_comment.permalink}).\n\n")
             message_builder.append(static.BOTTOM_REPLY_SECTION)
 
             message = "".join(message_builder)
