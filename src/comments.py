2c086c62bb6ff324d0250489608c57e49e3e95e2
Clemens Himmer <clemens.himmer@fusonic.net>
2021-03-05 17:34:06 +0100

    log comments

--- src/comments.py
+++ src/comments.py
@@ -72,6 +72,7 @@ def get_comment_body_details(comment_body):
 
 # TODO guess old posts wont be replyable so send a message instead then
 def reply_to_comment(reddit, comment_id, comment_reply):
+    print(f"Trying to reply to comment: {comment_id}, response: {comment_reply}")
     try:
         comment_to_be_replied_to = reddit.comment(id=comment_id)
         comment_to_be_replied_to.reply(comment_reply)
@@ -104,6 +105,7 @@ def process_comments(conn, reddit, comments):
         parent_comment = reddit.comment(id=comment_id).parent()
 
         if (static.COMMAND_LOWER in comment_body_lower and comment_author != static.REDDIT_USERNAME):
+            print(f"Found comment: {comment_id}, body: {comment_body_lower}")
             body_details = get_comment_body_details(comment_body_lower)
 
             comment_reply_builder = ["**Please do not use me yet, I'm not finished yet. Command may change, database cleared, etc**\n\n"]
